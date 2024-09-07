66c082eba2193eeec42243132d13bab1b56b71ff
Valeria Chubarova <chubarova.valeria99@gmail.com>
2024-09-07 16:06:08 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,35 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+document.body.id = "tree";
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    function createList(obj) {
+        const ul = document.createElement("ul");
+        for(const key in obj){
+            const li = document.createElement("li");
+            li.textContent = key;
+            ul.append(li);
+            if (Object.keys(obj[key]).length > 0) li.append(createList(obj[key]));
+        }
+        return ul;
+    }
+    element.append(createList(data));
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
